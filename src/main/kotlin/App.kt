8089d7d2caf17f134b6b13ee88bcff522cf5ac48
Mikko Suniala <mikko.suniala@iki.fi>
2020-09-12 13:52:01 +0300

    Components

---
+++ src/main/kotlin/App.kt
@@ -0,0 +1,37 @@
+import react.*
+import react.dom.div
+import react.dom.h3
+
+data class Video(val id: Int, val title: String, val speaker: String, val videoUrl: String)
+
+val unwatchedVideos = listOf(
+    Video(1, "Building and breaking things", "John Doe", "https://youtu.be/PsaFVLr8t4E"),
+    Video(2, "The development process", "Jane Smith", "https://youtu.be/PsaFVLr8t4E"),
+    Video(3, "The Web 7.0", "Matt Miller", "https://youtu.be/PsaFVLr8t4E")
+)
+
+val watchedVideos = listOf(
+    Video(4, "Mouseless development", "Tom Jerry", "https://youtu.be/PsaFVLr8t4E")
+)
+
+class App : RComponent<RProps, RState>() {
+    override fun RBuilder.render() {
+        div {
+            h3 {
+                +"Videos to watch"
+            }
+            // Method 1 for instantiating custom components.
+            child(VideoList::class) {
+                attrs.videos = unwatchedVideos
+            }
+
+            h3 {
+                +"Videos watched"
+            }
+            // Method 2 for instantiating custom components.
+            videoList {
+                videos = watchedVideos
+            }
+        }
+    }
+}
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,61 +1,9 @@
 import kotlinx.browser.document
-import kotlinx.css.*
-import kotlinx.html.HTMLTag
-import react.dom.*
-import styled.css
-import styled.styledDiv
-
-data class Video(val id: Int, val title: String, val speaker: String, val videoUrl: String)
-
-val unwatchedVideos = listOf(
-    Video(1, "Building and breaking things", "John Doe", "https://youtu.be/PsaFVLr8t4E"),
-    Video(2, "The development process", "Jane Smith", "https://youtu.be/PsaFVLr8t4E"),
-    Video(3, "The Web 7.0", "Matt Miller", "https://youtu.be/PsaFVLr8t4E")
-)
-
-val watchedVideos = listOf(
-    Video(4, "Mouseless development", "Tom Jerry", "https://youtu.be/PsaFVLr8t4E")
-)
+import react.dom.render
 
 fun main() {
     render(document.getElementById("root")) {
-        h1 {
-            +"KotlinConf Explorer"
+        child(App::class) {
         }
-        div {
-            h3 {
-                +"Videos to watch"
-            }
-            for (video in unwatchedVideos) {
-                videoItem(video)
-            }
-            h3 {
-                +"Videos watched"
-            }
-            for (video in watchedVideos) {
-                videoItem(video)
-            }
-        }
-        styledDiv {
-            css {
-                position = Position.absolute
-                top = 10.px
-                right = 10.px
-            }
-            h3 {
-                +"John Doe: Building and breaking things"
-            }
-            img {
-                attrs {
-                    src = "https://via.placeholder.com/640x360.png?text=Video+Player+Placeholder"
-                }
-            }
-        }
-    }
-}
-
-private fun RDOMBuilder<HTMLTag>.videoItem(video: Video) {
-    p {
-        +"${video.speaker}: ${video.title}"
     }
 }
